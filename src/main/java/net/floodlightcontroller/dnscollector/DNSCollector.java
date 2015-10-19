8016e36d126f6551c7e886a5bf5b13e3e72d8ddb
alberthitanaya <alberthitanaya@gmail.com>
2015-10-19 12:27:22 +1100

    initial commit

---
+++ src/main/java/net/floodlightcontroller/dnscollector/DNSCollector.java
@@ -0,0 +1,152 @@
+package net.floodlightcontroller.dnscollector;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketOut;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.dnscollector.web.DNSCollectorWebRoutable;
+import net.floodlightcontroller.packet.Data;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.UDP;
+import net.floodlightcontroller.restserver.IRestApiService;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.TransportPort;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+public class DNSCollector implements IOFMessageListener, IFloodlightModule, IDNSCollectorService {
+	protected IFloodlightProviderService floodlightProvider;
+	protected IRestApiService restApiService;
+	protected ArrayList<Map<String,Object>> DNSqueries = new ArrayList<Map<String,Object>>();
+	
+	@Override
+	public String getName() {
+		return DNSCollector.class.getSimpleName();
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IDNSCollectorService.class);
+		return l;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		Map<Class<? extends IFloodlightService>, IFloodlightService> m = new HashMap<Class<? extends IFloodlightService>, IFloodlightService>();
+		m.put(IDNSCollectorService.class, this);
+		return m;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		Collection<Class<? extends IFloodlightService>> l = 
+				new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		l.add(IRestApiService.class);
+		return l;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		restApiService = context.getServiceImpl(IRestApiService.class);
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		restApiService.addRestletRoutable(new DNSCollectorWebRoutable());
+	}
+
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
+			FloodlightContext cntx) {
+		switch (msg.getType()){
+		case PACKET_IN:
+			Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+			if (eth.getEtherType() == EthType.IPv4) {
+				IPv4 ipv4 = (IPv4) eth.getPayload();
+				if(ipv4.getProtocol().equals(IpProtocol.UDP)){
+					UDP udp = (UDP) ipv4.getPayload();
+					TransportPort dstPort = udp.getDestinationPort();
+					int port = dstPort.getPort();
+					if (port == 53) { //it's a DNS PACKET!
+						Data dataPkt = (Data) udp.getPayload();
+						byte[] arr = dataPkt.getData();
+						StringBuilder strBuilder = new StringBuilder();
+						for(int i = 0; i < dataPkt.getData().length; i++) {
+							strBuilder.append((char)arr[i]);
+						}
+						Map<String,Object> hm = new HashMap<String, Object>();
+						hm.put("query", strBuilder.toString());
+						hm.put("mac", eth.getSourceMACAddress().toString());
+						hm.put("switch", sw.getId().toString());
+						hm.put("time", System.currentTimeMillis());
+						this.DNSqueries.add(hm);
+						//byte[] serializedData = eth.serialize();
+//						OFPacketOut po = sw.getOFFactory().buildPacketOut()
+//								.setData(serializedData)
+//								.setActions(Collections.singletonList((OFAction) sw.getOFFactory().actions().output(OFPort.FLOOD, 0xffFFffFF)))
+//							    .setInPort(OFPort.CONTROLLER)
+//							    .build();
+//						sw.write(po);
+					} else {
+						break; // NOT A DNS PACKET
+					}
+				}
+			}
+		default:
+			break;
+		}
+		return Command.CONTINUE;
+	}
+	
+	@Override 
+	public ArrayList<Map<String,Object>> getDNSqueries() {
+		ArrayList<Map<String,Object>> result = new ArrayList<Map<String,Object>>();	
+		result = (ArrayList<Map<String,Object>>) DNSqueries.clone();
+		DNSqueries.clear();
+		return result;
+	}
+}
